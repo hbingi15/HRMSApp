94da0feb852afc47e356712567922adaad8b1123
Haritha Bingi <hbingi49@gmail.com>
2022-09-16 12:48:20 +0530

    Identity Tables Generate

---
+++ HRMSApplication/Identity/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRMSApplication.Identity
+{
+    //ApplicationUser class,we extend the IdentityUser class with  additional properties
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte UserLevel { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
+
--- HRMSApplication/Program.cs
+++ HRMSApplication/Program.cs
@@ -1,7 +1,10 @@
 using HRMSApplication.Contracts;
 using HRMSApplication.DapperORM;
+using HRMSApplication.Identity;
 using HRMSApplication.Repository;
 using HRMSApplication.Utilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using NLog;
 using NLog.Web;
 
@@ -23,9 +26,17 @@ builder.Services.AddTransient<ILoggerManager, LoggerManager>();
 builder.Services.AddTransient<IEmployee, EmployeeRepository>();
 builder.Services.AddSingleton<IAdminEmployee, AdminEmployeeRepository>();
 
-
-
 builder.Services.AddTransient<EmployeeDapperContext>();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//AddEntityFrameworkStores method to register the required EF Core implementation of Identity stores
+builder.Services.AddIdentity<ApplicationUser, Microsoft.AspNetCore.Identity.IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+
 
 var app = builder.Build();
 
