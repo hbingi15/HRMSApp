2904378296a015055bc0f06dac74fcf15673a401
Chamu Chilakala <chamu.ch@outlook.com>
2022-09-22 12:41:06 +0530

    commit

--- HRMSApplication/Models/Entity/EmpInductionDocEntity.cs
+++ HRMSApplication/Models/Entity/EmpInductionDocEntity.cs
@@ -8,10 +8,10 @@
             public string emid_document { get; set; }
             public int emid_processed_ausr_id { get; set; }
             public int emid_verified { get; set; }
-        public IFormFile Resume { get; set; }
-        public IFormFile SSCCertificates { get; set; }
-        public IFormFile Intercertificates { get; set; }
-        public IFormFile BtechCertificate { get; set; }
+            public IFormFile Resume { get; set; }
+            public IFormFile SSCCertificates { get; set; }
+            public IFormFile Intercertificates { get; set; }
+            public IFormFile BtechCertificate { get; set; }
 
 
     }
