3226b59e101fd0bdd3442f3924444c4d1f3777f1
Haritha Bingi <hbingi49@gmail.com>
2022-09-23 14:30:46 +0530

    User login

--- HRMSApplication/Identity/ApplicationUser.cs
+++ HRMSApplication/Identity/ApplicationUser.cs
@@ -5,9 +5,7 @@ namespace HRMSApplication.Identity
 {
     //ApplicationUser class,we extend the IdentityUser class with  additional properties
     public class ApplicationUser : IdentityUser
-    {/// <summary>
-    /// ///
-    /// </summary>
+    {
         [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
