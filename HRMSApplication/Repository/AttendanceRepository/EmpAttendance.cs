a8294e57c042a9df6b60dfc0fc6b2dfb4565f8ea
Haritha Bingi <hbingi49@gmail.com>
2022-10-02 20:52:06 +0530

    All changes

--- HRMSApplication/Repository/AttendanceRepository/EmpAttendance.cs
+++ HRMSApplication/Repository/AttendanceRepository/EmpAttendance.cs
@@ -86,12 +86,12 @@ namespace HRMSApplication.Repository.AttendanceRepository
                 {
                     conn.Open();
                     log.LogInfo("Calculate Day Attendance of Particular Employee");
-                    dayattend = (List<DayAttendance>)conn.Query<DayAttendance>(query, new {@id = da.Emp_Id, @dtid = da.day});
+                    dayattend = (List<DayAttendance>)conn.Query<DayAttendance>(query, new { @id = da.Emp_Id, @dtid = da.day });
                     //totalhours = conn.Execute(query2);
 
                     foreach (var attendance in dayattend)
                     {
-                        punchingHours = conn.ExecuteScalar<int>(query1, new { @eatn_punch_in = attendance.eatn_punch_in, @eatn_punch_out = attendance.eatn_punch_out, @id= da.Emp_Id, @dtid=da.day});
+                        punchingHours = conn.ExecuteScalar<int>(query1, new { @eatn_punch_in = attendance.eatn_punch_in, @eatn_punch_out = attendance.eatn_punch_out, @id = da.Emp_Id, @dtid = da.day });
                         totalhours = totalhours + punchingHours;
                         tot_hours = Math.Abs(totalhours);
                     }
@@ -104,40 +104,7 @@ namespace HRMSApplication.Repository.AttendanceRepository
                 throw msg;
             }
         }
-        //overloading CalculateDayAttendance  method
-        public int CalculateDayAttendance(int id,DateTime dt)
-        {
-            string query = "select * from EmployeeAttendance where empl_id=@id and date_id=@dtid";
-            string query1 = "select date_part('hour', @eatn_punch_out::timestamp - @eatn_punch_in::timestamp) from employeeattendance where empl_id=@id and date_id=@dtid";
-            // string query2 = "select sum(date_part('hour',eatn_punch_in::timestamp - eatn_punch_out::timestamp)) from EmployeeAttendance where empl_id = " + id;
-            int totalhours = 0;
-            int punchingHours = 0;
-            int tot_hours = 0;
-            IEnumerable<DayAttendance> dayattend = null;
-            try
-            {
-                using (var conn = edc.CreateConnection())
-                {
-                    conn.Open();
-                    log.LogInfo("Calculate Monthly Attendance of Particular Employee");
-                    dayattend = (List<DayAttendance>)conn.Query<DayAttendance>(query, new { @id = id, @dtid = dt });
-                    //totalhours = conn.Execute(query2);
 
-                    foreach (var attendance in dayattend)
-                    {
-                        punchingHours = conn.ExecuteScalar<int>(query1, new { @eatn_punch_in = attendance.eatn_punch_in, @eatn_punch_out = attendance.eatn_punch_out, @id = id, @dtid = dt });
-                        totalhours = totalhours + punchingHours;
-                        tot_hours = Math.Abs(totalhours);
-                    }
-                    return tot_hours;
-
-                }
-            }
-            catch (Exception msg)
-            {
-                throw msg;
-            }
-        }
         //--Monthly Attendance of Particular Employee
         public MonthAttendanceEntity CalculateMonthAttendance(DayAttendanceEntity da)
         {
@@ -148,28 +115,31 @@ namespace HRMSApplication.Repository.AttendanceRepository
 
             //--Query for getting ending day of given date
             string query2 = "SELECT (date_trunc('month', @gdt::date) + interval '1 month' - interval '1 day')::date";
-           
+
             //--Query for getting holidays of given date(month)
             string query3 = " select count(*) from holidays where hday_date>=@st and hday_date<=@et";
-           
+
             DateTime stdt, endt;
-            int datattend,i = 0,actual_attendance=0,NoOfHolidays; 
+            int datattend, i = 0, actual_attendance = 0, NoOfHolidays;
             int[] mon_atd = new int[31];
             //monatd[0] = 2;
-            
+
             try
             {
                 using (var conn = edc.CreateConnection())
                 {
                     conn.Open();
                     log.LogInfo("Calculate Monthly Attendance of Particular Employee");
-                    stdt = conn.ExecuteScalar<DateTime>(query1, new {@gdt=da.day});
+                    stdt = conn.ExecuteScalar<DateTime>(query1, new { @gdt = da.day });
                     endt = conn.ExecuteScalar<DateTime>(query2, new { @gdt = da.day });
-                    NoOfHolidays = conn.ExecuteScalar<int>(query3, new { @st = stdt,@et=endt });
+                    NoOfHolidays = conn.ExecuteScalar<int>(query3, new { @st = stdt, @et = endt });
+                    DayAttendanceEntity dayobj = new DayAttendanceEntity();
                     while (stdt < endt)
                     {
                         //--Calling Employee Day attendance
-                        datattend = CalculateDayAttendance(da.Emp_Id,stdt);
+                        dayobj.Emp_Id = da.Emp_Id;
+                        dayobj.day = stdt;
+                        datattend = CalculateDayAttendance(dayobj);
                         stdt = stdt.AddDays(1);
                         //--Assign Day attendance to array
                         mon_atd[i] = datattend;
@@ -185,7 +155,7 @@ namespace HRMSApplication.Repository.AttendanceRepository
                     }
                 }
                 //
-                ma.NoOfDays = i+1;
+                ma.NoOfDays = i + 1;
                 ma.NoOfHolidays = NoOfHolidays;
                 ma.Actual_Attended_days = actual_attendance;
                 ma.Mon_Atd = mon_atd;
